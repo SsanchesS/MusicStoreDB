529b02abc3aa9ccab0a9df5b6d6ea7c06fbdda4c
Alexander <100308427+SsanchesS@users.noreply.github.com>
2023-10-07 14:43:38 +0300

    сделал сервер
    должен работать я не проверял )

--- MusicStore/src/resolvers/check_login.py
+++ MusicStore/src/resolvers/check_login.py
@@ -1,6 +1,6 @@
-from sql_base.models import usersM
+from sql_base.models import CustomersM 
 from sql_base.base import base_worker
 
-def check_login_request(user: usersM):
-    post_id = base_worker.insert_data(f"SELECT id FROM user WHERE username = ? AND password = ?",(user.username,user.password))
-    return post_id
+def check_login_request(сustomers: CustomersM):
+    customer_id = base_worker.insert_data(f"SELECT customer_id FROM Customers WHERE email = ? AND phone_number = ?",(сustomers.email,сustomers.phone_number))
+    return customer_id
--- MusicStore/src/routers/login.py
+++ MusicStore/src/routers/login.py
@@ -1,18 +1,17 @@
-from fastapi import APIRouter
-from sql_base.models import usersM
+from fastapi import APIRouter, HTTPException
+from sql_base.models import CustomersM
 from resolvers.check_login import check_login_request
 
 login_router = APIRouter()
-
 
 @login_router.get('/')
 def not_login():
     return {"Message": "Login in system"}
 
 @login_router.post('/log')
-def check_login1(user: usersM):
-    id = check_login_request(user)
-    if id:
-        return {"code": 200, "message": "Login correct", 'id': id}
+def check_login1(user: CustomersM):
+    user_id = check_login_request(user)
+    if user_id:
+        return {"code": 200, "message": "Login correct", 'user_id': user_id}
     else:
-        return {"code": 401, "message": "Login incorrect, try again", 'id': None}
+        raise HTTPException(status_code=401, detail="Login incorrect, try again")
